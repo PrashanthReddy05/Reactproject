ab6139de54584138855823e26e88fab2a4877c27
PrashanthReddy05 <bembadiprashanthreddy@gmail.com>
2023-01-20 12:27:17 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Message from './Message.js';
+//import Navbar from './Navbar.js'
+
+const App= () => {
+  return (
+    <div>
+      <Message />
+    </div>
+  );
+}
+
+export default App;
